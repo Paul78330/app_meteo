e2df37881b2c96344ed7fe64d6c35e9faa409d24
paul-gaultier <paul.gaultier76@yahoo.com>
2023-05-09 10:55:26 +0200

    partie2

--- weather/Row.js
+++ weather/Row.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
+import moment from "moment/moment";
+import 'moment/locale/fr'
+
+moment.locale('fr')
+
 
 export default class WeatherRow extends React.Component {
     static propTypes = {
-        day: PropTypes.array
+        day: PropTypes.array,
+        index: PropTypes.number
     }
 
     render() {
@@ -17,9 +23,10 @@ export default class WeatherRow extends React.Component {
         const weatherData = day.map((dayData) => {
             const { temp, feels_like, weather, dt } = dayData;
 
+
             return (
                 <View key={dt}>
-                    <Text>Date: {new Date(dt * 1000).toLocaleDateString()}</Text>
+                    <Text>Date: {new Date(dt * 1000).toLocaleDateString('fr-FR')}</Text>
                     <Text>Température : {temp.day}°C</Text>
                     <Text>Sensation de température : {feels_like.day}°C</Text>
                     <Text>Conditions météorologiques : {weather[0].description}</Text>
